4a4735a512bfc9d7260545c969874555ec42b57e
xdassie <50299712+xdassie@users.noreply.github.com>
2020-02-02 18:37:00 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir -p /home/gerbera/.config/gerbera/
 gerbera --create-config > /home/gerbera/.config/gerbera/config.xml
-/sbin/tini -- gerbera -p 49152
+/sbin/tini -- gerbera -p 49152 --add-file=/scan
